cc8e5b6492bac5614c4cc8ceeafa04bdcdbfdda1
kauly <kauly@hotmail.com>
2020-02-28 18:59:11 -0300

    w custom widget

---
+++ public/admin/scripts/widget.js
@@ -0,0 +1,28 @@
+var CategoriesControl = createClass({
+  handleChange: function(e) {
+    this.props.onChange(e.target.value.split(",").map(e => e.trim()));
+  },
+
+  render: function() {
+    var value = this.props.value;
+    return h("input", {
+      type: "text",
+      value: value ? value.join(", ") : "",
+      onChange: this.handleChange
+    });
+  }
+});
+
+var CategoriesPreview = createClass({
+  render: function() {
+    return h(
+      "ul",
+      {},
+      this.props.value.map(function(val, index) {
+        return h("li", { key: index }, val);
+      })
+    );
+  }
+});
+
+CMS.registerWidget("teste", CategoriesControl, CategoriesPreview);
