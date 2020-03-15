c64aecc7a13af9308da312d0f75bfff3c07b6533
kauly <kauly@hotmail.com>
2020-03-15 10:40:58 -0300

    testing custom lib

--- public/admin/scripts/widget.js
+++ public/admin/scripts/widget.js
@@ -1,3 +1,4 @@
-import { Control, Preview } from "@ncwidgets/file-relation";
+//import { Control, Preview } from "@ncwidgets/file-relation";
+const { Control, Preview } = require("@ncwidgets/file-relation");
 
 CMS.registerWidget("file-relation", Control, Preview);
