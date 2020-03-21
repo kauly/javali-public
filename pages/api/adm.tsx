d8e3de9bc5682e9278d71456e8e8c218fbd4c274
kauly <kauly@hotmail.com>
2020-03-21 17:56:02 -0300

    yml products

--- pages/api/adm.tsx
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.writeHead(301, {
-    Location: "http://localhost:3000/admin/index.html"
-  });
-  return res.end();
-};
